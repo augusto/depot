354afc526671dffa70467066343707f0be40d12b
Augusto Rodriguez <augusto.rodriguez@gmail.com>
2011-01-30 09:58:50 +0000

    added unit tests

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+  
   # Replace this with your real tests.
   test "the truth" do
     assert true
@@ -23,14 +25,43 @@ class ProductTest < ActiveSupport::TestCase
     product.price = -1 
     assert product.invalid? 
     assert_equal "must be greater than or equal to 0.01", 
-      product.errors[:price].join('; ')
+      product.errors[:price].join
     
     product.price = 0 
     assert product.invalid? 
     assert_equal "must be greater than or equal to 0.01",
-      product.errors[:price].join('; ')
+      product.errors[:price].join
     
     product.price = 1
     assert product.valid?
   end
+  
+  test "image url" do 
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.JPg}
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+    
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} should be valid"
+    end
+    
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} should be invalid"
+    end
+  end
+  
+  def new_product(image_url)
+      Product.new( :title => "Book Title",
+                   :description => "description",
+                   :price => 1,
+                   :image_url => image_url)
+  end
+  
+  test "product is not valid without an unique title" do
+    product = Product.new( :title => products(:ruby).title,
+                           :description => "yyy",
+                           :price => 1,
+                           :image_url => "aarc.jpg")
+    assert !product.save
+    assert_equal "has already been taken", product.errors[:title].join
+    end
 end
