64cf3104bd20ec4a24068d9fdee0c8c9936838b8
Augusto Rodriguez <augusto.rodriguez@gmail.com>
2011-01-30 16:12:51 +0000

    Iteration D - playtime

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -43,6 +43,7 @@ class LineItemsController < ApplicationController
     @cart = current_cart
     product = Product.find(params[:product_id])
     @line_item = @cart.line_items.build( :product => product)
+    session[:counter] = 0;
 
     respond_to do |format|
       if @line_item.save
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,8 @@
 class StoreController < ApplicationController
   def index
     @products = Product.all
+    session[:counter] ||= 0
+    session[:counter] += 1
   end
 
 end
