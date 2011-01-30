61450829a9d66776f04e292df9283514f278d8f2
Augusto Rodriguez <augusto.rodriguez@gmail.com>
2011-01-30 09:32:26 +0000

    added autotest and unit tests

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -5,4 +5,32 @@ class ProductTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
+  
+  test "product attributes must not be empty" do 
+    product = Product.new 
+    assert product.invalid? 
+    assert product.errors[:title].any?
+    assert product.errors[:description].any? 
+    assert product.errors[:price].any? 
+    assert product.errors[:image_url].any?
+  end
+  
+  test "product price must be positive" do
+    product = Product.new(:title => "My Book Title",
+                          :description => "yyy",
+                          :image_url => "zzz.jpg")
+                          
+    product.price = -1 
+    assert product.invalid? 
+    assert_equal "must be greater than or equal to 0.01", 
+      product.errors[:price].join('; ')
+    
+    product.price = 0 
+    assert product.invalid? 
+    assert_equal "must be greater than or equal to 0.01",
+      product.errors[:price].join('; ')
+    
+    product.price = 1
+    assert product.valid?
+  end
 end
