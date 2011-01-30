7ac285d66c1d9fbb0525c9de68f10668c46061f9
Augusto Rodriguez <augusto.rodriguez@gmail.com>
2011-01-30 11:29:43 +0000

    Iteration C4 - playtime

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -15,6 +15,8 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select '#product_list'
+    assert_select '.list_actions a', :minimum => 3
   end
 
   test "should get new" do
